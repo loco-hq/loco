fe43f90bf21c4afdd681114a1f49086e265990e4
Ben Hubbard <55447225+humandad@users.noreply.github.com>
2026-04-24 08:07:53 -0400

    move adapters

--- loco-lake/crates/loco-lake/src/lib.rs
+++ loco-lake/crates/loco-lake/src/lib.rs
@@ -2,12 +2,11 @@ pub mod value;
 pub mod error;
 pub mod record;
 pub mod adapter;
-pub mod memory;
-pub mod sqlite;
+pub mod adapters;
 
 pub use value::Value;
 pub use error::Error;
 pub use record::Record;
 pub use adapter::DataAdapter;
-pub use memory::InMemoryAdapter;
-pub use sqlite::SqliteAdapter;
+pub use adapters::memory::InMemoryAdapter;
+pub use adapters::sqlite::SqliteAdapter;
